d1b93431ce53aec3ce8fe9d9883b0685159f76ed
nhylee <nhyen875@gmail.com>
2025-09-18 23:42:29 +0700

    commit
    aaaa

---
+++ BT_Day04/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BT_Day04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
